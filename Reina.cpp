6e5300d772b0b71af7a3a017db8dd93a7d4d1316
Edilson Gonzalez <edilson.gonza@unitec.edu>
2012-12-14 16:05:15 -0600

    Validaciones 2

--- Reina.cpp
+++ Reina.cpp
@@ -7,8 +7,6 @@ Reina::Reina(ENUM_COLOR color): Pieza(color){
 }
 
 bool Reina::esMovimientoValido(Posicion* origen, Posicion* destino ){
-	/*Posicion x = Posicion(origen->getX(),origen->getY()); 	
- 	Posicion y = Posicion(origen->getX(),origen->getY());*/
 	Alfil temp;
 	Torre temp2;		
 	if(temp.esMovimientoValido(origen, destino) || temp2.esMovimientoValido(origen, destino))
